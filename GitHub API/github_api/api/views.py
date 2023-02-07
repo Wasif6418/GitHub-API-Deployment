7015980470c460a93c10fdf2c0a4a77b962baade
wasifmohammad6@gmail.com <Administrator@Tanveer-47735.wgbd.com>
2023-02-07 10:46:30 +0600

    Project upload

---
+++ GitHub API/github_api/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+import requests
+def index(request):
+    user= False
+    if request.method == 'POST':
+        username= request.POST.get('username')
+        url= 'https://api.github.com/users/' + str(username)
+        response = requests.get(url)
+        user= response.json()
+    return render(request, 'index.html',  context={'user': user})
+# Create your views here.
